c875e6f547692d11bab536130f245bf258f335ff
dtroy345 <59236999+dtroy345@users.noreply.github.com>
2023-11-04 23:39:58 -0400

    attempt at the toggle

--- script.js
+++ script.js
@@ -1,23 +1,28 @@
 // hide and unhide private message
-var nextStep = document.querySelector('#nextStep');
+// var nextStep = document.querySelector('#nextStep');
 
-nextStep.addEventListener('click', function (e) {
-  e.preventDefault();
-  // Hide first view
-  document.getElementById('form').style.display = 'none';
+// nextStep.addEventListener('click', function (e) {
+//   e.preventDefault();
+//   // Hide first view
+//   document.getElementById('form').style.display = 'none';
 
-  // Show thank you message element
-  document.getElementById('completed').style.display = 'block';
-});
+//   // Show thank you message element
+//   document.getElementById('completed').style.display = 'block';
+// });
 
 // color change for buttons
-$(".unread").on('click', function () {
-    var x = $(this).attr('class');
-    if (x == 'unread') {
-        $(this).removeClass("unread");
-        $(this).addClass("read");
-    } else {
-        $(this).removeClass("read");
-        $(this).addClass("unread");
-    }
-});
+// $(".unread").on('click', function () {
+//     var x = $(this).attr('class');
+//     if (x == 'unread') {
+//         $(this).removeClass("unread");
+//         $(this).addClass("read");
+//     } else {
+//         $(this).removeClass("read");
+//         $(this).addClass("unread");
+//     }
+// });
+
+// color change for button another try
+function colorchange() {
+    document.getElementsById("unreade").style.backgroundColor = "red";
+}
