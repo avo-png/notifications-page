a0a354edfd8a07f37815267427ac8e51bb9841c4
dtroy345 <59236999+dtroy345@users.noreply.github.com>
2023-11-04 23:45:26 -0400

    updates

--- script.js
+++ script.js
@@ -23,6 +23,18 @@
 // });
 
 // color change for button another try
-function colorchange() {
-    document.getElementsById("unreade").style.backgroundColor = "red";
-}
+// function colorchange() {
+//     document.getElementsById("unreade").style.backgroundColor = "red";
+// }
+
+// updates
+var nextStep = document.querySelector('#nextStep');
+
+nextStep.addEventListener('click', function (e) {
+  e.preventDefault();
+  // Hide first view
+  document.getElementById('form').style.display = 'none';
+
+  // Show thank you message element
+  document.getElementById('completed').style.display = 'block';
+});
