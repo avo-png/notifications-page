2fc132248cd7f3e60822808c01a97eced65af160
dtroy345 <59236999+dtroy345@users.noreply.github.com>
2023-11-05 21:51:26 -0500

    i almost got it

--- script.js
+++ script.js
@@ -1,15 +1,3 @@
-// hide and unhide private message
-// var nextStep = document.querySelector('#nextStep');
-
-// nextStep.addEventListener('click', function (e) {
-//   e.preventDefault();
-//   // Hide first view
-//   document.getElementById('form').style.display = 'none';
-
-//   // Show thank you message element
-//   document.getElementById('completed').style.display = 'block';
-// });
-
 // color change for each thing
 $(".unread").on('click', function () {
     var x = $(this).attr('class');
@@ -22,30 +10,31 @@ $(".unread").on('click', function () {
     }
 });
 
-
-// updates
-// var changeColors = document.querySelector('#mButton');
-
-// changeColors.addEventListener('click', function (e) {
-//   e.preventDefault();
-//   // change to white
-//   document.getElementById('unread').backgroundColor = 'rgb(26,255,0)';
-
-//   // change to red
-//   document.getElementById('unread').style.backgroundColor = 'rgb(255,145,0)';
+// change colors on all when click button
+// $(".all-button").on('click', function () {
+//     var y = 
+//     if (x == 'unread') {
+//         $(this).removeClass("unread");
+//         $(this).addClass("read");
+//     } else {
+//         $(this).removeClass("read");
+//         $(this).addClass("unread");
+//     }
 // });
 
-// function colorchange(id)
-// {
-//   var background = document.getElementById(id).style.backgroundColor;
+// function changeColors() {
+//   var background = document.querySelectorAll('#umread').style.backgroundColor;
 
-//   if(background == "rgb(255, 145, 0)")
+//   if(background == "hsl(211, 68%, 94%)")
 //   {
-//   document.getElementById(id).style.backgroundColor = "rgb(26,255,0)";
+//     document.querySelectorAll('#umread').style.backgroundColor = "rgb(26,255,0)";
 //   }
 //   if(background == "rgb(26, 255, 0)")
 //   {
-//     document.getElementById(id).style.backgroundColor = "rgb(255,145,0)";
+//     document.querySelectorAll('#umread').style.backgroundColor = "hsl(211, 68%, 94%)";
 //   }
-
 // }
+
+// document.getElementById("allButton").addEventListener("click", function(){ 
+//     document.querySelectorAll("unread").style.backgroundColor = "black";
+// }); 
